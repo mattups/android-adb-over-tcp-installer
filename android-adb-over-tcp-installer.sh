fa33354c926d696b93db7883cf62feb166656df1
Mattia Tupone <tupone.mattia@gmail.com>
2017-10-05 17:41:36 +0200

    Addedd creation of log_timestamp.txt. Change adb connection logic

--- android-adb-over-tcp-installer.sh
+++ android-adb-over-tcp-installer.sh
@@ -3,26 +3,29 @@
 #Change the ip_address file to add or delete targets
 #Place your_package.apk in the same script folder
 
-#Getting timestamp for the log
+#Creating log with timestamp filename
 timestamp_date=$(date +%F_%T)
-echo -e "====== Upgrade started on "$timestamp_date" ======\n" >> log.txt
+touch log_"$timestamp_date".txt
+
+echo -e "====== Process started on "$timestamp_date" ======\n" >> log_$timestamp_date.txt
 
 #Getting addresses to install your_package.apk
+#TODO Define logic for ADB
 cat ip_address_list.txt |  while read target
     do
         ping -c 1 "$target" >> /dev/null
         if [ $? -eq 0 ]; then
-            adb connect "$target";
-            adb install your_package.apk;
+            adb connect "$target" &&
+            adb install your_package.apk &&
             adb disconnect;
-            echo "Installed on" $target "- $timestamp_date" >> log.txt;
+            echo "Installed on" $target "- $timestamp_date" >> log_$timestamp_date.txt;
         else
-            echo "Failed on" $target "- $timestamp_date" >> log.txt;
-            echo "Upgrade on $target failed, see log";
+            echo "Installation failed on" $target "- $timestamp_date" >> log_$timestamp_date.txt;
+            echo "Installation on $target failed, see log";
         fi
     done
 
 echo -e "Done."
 
 #Closing log
-echo -e "\n====== Log for upgrade on" $timestamp_date " ======\n" >> log.txt
+echo -e "\n====== Process finished on "$timestamp_date" ======\n" >> log_$timestamp_date.txt
